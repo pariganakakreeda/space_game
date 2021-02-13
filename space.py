0b3034b92312b5fde3fb7057b80f44d2af136b88
Hamnah Sabry <hamnahsabry@gmail.com>
2021-02-13 21:05:24 +0100

    one meteor moving

--- space.py
+++ space.py
@@ -22,6 +22,13 @@ class Meteor(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load(path)
         self.rect = self.image.get_rect(center = (x_pos,y_pos))
+        self.x_speed = x_speed
+        self.y_speed = y_speed
+
+    def update(self):
+        self.rect.centerx += self.x_speed
+        self.rect.centery += self.y_speed
+
 
 pygame.init() # initiate pygame
 screen = pygame.display.set_mode((1280,720)) #Create display surface
@@ -31,7 +38,7 @@ spaceship = SpaceShip('spaceship.png',640,500,10)
 spaceship_group = pygame.sprite.GroupSingle()
 spaceship_group.add(spaceship)
 
-meteor1 = Meteor('Meteor1.png',400,400,1,1)
+meteor1 = Meteor('Meteor1.png',400,-100,1,4)
 meteor_group = pygame.sprite.Group()
 meteor_group.add(meteor1)
 
@@ -46,5 +53,6 @@ while True: # Game loop
     spaceship_group.draw(screen)
     meteor_group.draw(screen)
     spaceship_group.update()
+    meteor_group.update()
     pygame.display.update() # Draw frame
     clock.tick(120) # Control the framerate
